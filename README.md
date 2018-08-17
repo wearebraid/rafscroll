c53759d970bbfb11755d2d18e2e5254f46e0fc3a
Justin Schroeder <justin@wearebraid.com>
2018-08-17 12:08:46 -0400

    Adds documentation in preperation of initial npm release

---
+++ README.md
@@ -0,0 +1,84 @@
+# Rafscroll
+
+Rafscroll is a small library to scroll windows and DOM elements by using a
+browser’s native `requestAnimationFrame`, ensuring optimal scrolling without
+impacting site performance. Rafscroll can operate on both the x and y axis
+with various easing functions.
+
+[Demo here](https://wearebraid.com)
+
+## Get Started
+
+_Note: This readme assumes you are using a build tool like [webpack](https://webpack.js.org/)_
+
+First install from NPM or yarn:
+
+```sh
+npm install rafscroll
+- or -
+yarn add rafscroll
+```
+
+Then import/require the library in your front end code:
+
+```js
+// main.js
+
+import rafscroll from 'rafscroll'
+```
+
+Rafscroll exports a function. Calling this function creates a new instance of
+rafscroll.
+
+```js
+let scroller = rafscroll(element, {...options})
+```
+
+The returned object can then have it's X or Y axis animated by calling
+`scrollTop()` or/and `scrollLeft()`. The return value of either of these
+is a `Promise` that resolves when the scroll animation is complete.
+For convenience rafscroll also uses some intelligent defaults — the window is
+the default element, and the the default easing is quadratic.
+
+A full example:
+
+```js
+import rafscroll from 'rafscroll'
+
+// Scroll 100px down the page after 1 second.
+setTimeout(async () => {
+    await rafscroll().scrollTop(100)
+    alert('Finished scrolling')
+}, 1000)
+```
+
+## Options
+
+Available options to pass as a second argument to rafscroll:
+
+Option   | Values                                        | Default
+---------|-----------------------------------------------|----------------------------
+duration | any `Number` in milliseconds                  | 500
+easing   | 'quadratic', 'linear', or a custom `Function` | 'quadratic'
+
+Custom easing functions receive an object containing 4 properties:
+
+```js
+{current, duration, posDelta, posStart}
+```
+
+They should return a numeric value representing the current position. For
+example the linear easing function already included in rafscroll is:
+
+```js
+function ({current, duration, posDelta, posStart}) {
+    return (current / duration) * posDelta + posStart
+}
+```
+
+## Sponsor
+
+Rafscroll is written and maintained by [Braid LLC](https://www.wearebraid.com),
+and offered freely under an MIT license.
+
+[<img src="https://assets.wearebraid.com/sig.png" title="Written and maintained by Braid LLC" alt="Braid LLC" width="150" height="44">](https://www.wearebraid.com)
--- src/rafscroll.js
+++ src/rafscroll.js
@@ -5,6 +5,12 @@
  * @param  {options}
  */
 class RequestAnimationFrameScroll {
+  /**
+   * Instantiate a new scroll RequestAnimationFrameScroll instance.
+   *
+   * @param {DOMElement} el
+   * @param {Object} options
+   */
   constructor (el, options = {}) {
     this.options = Object.assign({
       duration: 500,
@@ -20,6 +26,12 @@ class RequestAnimationFrameScroll {
     this._promise = Promise.resolve()
   }
 
+  /**
+   * Perform the scroll animation and return a promise to indicate when the
+   * scroll is complete.
+   *
+   * @return {Promise}
+   */
   animate () {
     if (!this._ticking) {
       this._promise = new Promise((resolve, reject) => {
@@ -31,14 +43,34 @@ class RequestAnimationFrameScroll {
     return this._promise
   }
 
+  /**
+   * Scroll the X coordinate.
+   *
+   * @param {Number} target 
+   * @param {Number} duration 
+   */
   scrollLeft (target, duration) {
     return this.createScroll(target, duration, 'x')
   }
 
+  /**
+   * Scroll the Y coordinate.
+   *
+   * @param {Number} target 
+   * @param {Number} duration 
+   */
   scrollTop (target, duration) {
     return this.createScroll(target, duration)
   }
 
+  /**
+   * Create a scroll "object" that describes all the information about our
+   * scroll animation.
+   *
+   * @param {Number} target
+   * @param {Number} duration
+   * @param {String} axis
+   */
   createScroll (target, duration, axis = 'y') {
     let prop = axis === 'x' ? 'scrollLeft' : 'scrollTop'
     let start = this.el[prop]
@@ -56,6 +88,11 @@ class RequestAnimationFrameScroll {
     return this.animate()
   }
 
+  /**
+   * Get the easing function.
+   * 
+   * @return {Function}
+   */
   getEasing () {
     let map = {
       quadratic: this._quadratic,
@@ -73,16 +110,34 @@ class RequestAnimationFrameScroll {
     return this._quadratic
   }
 
+  /**
+   * Set the easing function.
+   *
+   * @param {Function} easing
+   * @return {self}
+   */
   setEasing (easing) {
     this.options.easing = easing
     return this
   }
 
+  /**
+   * Set the duration in milliseconds.
+   *
+   * @param {Number} duration
+   * @return {self}
+   */
   setDuration (duration) {
     this.options.duration = duration
     return this
   }
 
+  /**
+   * Recursively tick through our duration with eased scrolling.
+   *
+   * @param {Function} ticker
+   * @return {null}
+   */
   _tick (ticker) {
     if (this._ticking) {
       window.requestAnimationFrame(() => {
@@ -99,12 +154,25 @@ class RequestAnimationFrameScroll {
     }
   }
 
+  /**
+   * Determine if the scroll is complete.
+   *
+   * @param {Number} ticker
+   * @return {Boolean}
+   */
   _expired (ticker) {
     let y = !this.scrolls.y.timeEnd || this.scrolls.y.timeEnd < ticker
     let x = !this.scrolls.x.timeEnd || this.scrolls.x.timeEnd < ticker
     return y && x
   }
 
+  /**
+   * Set a property on the element for the given axis. Typically this is
+   * either the top or left properties.
+   *
+   * @param {Object} scroll
+   * @param {Number} ticker 
+   */
   _setprop (scroll, ticker) {
     if (!scroll.timeEnd) {
       return false
@@ -117,10 +185,20 @@ class RequestAnimationFrameScroll {
     }
   }
 
+  /**
+   * Linear easing function.
+   *
+   * @param {Object} object 
+   */
   _linear ({current, duration, posDelta, posStart}) {
     return (current / duration) * posDelta + posStart
   }
 
+  /**
+   * Quadratic easing function.
+   *
+   * @param {Object} object 
+   */
   _quadratic ({current, duration, posDelta, posStart}) {
     var r = current / (duration / 2)
     if (r < 1) {
@@ -130,6 +208,11 @@ class RequestAnimationFrameScroll {
     return -posDelta / 2 * (r * (r - 2) - 1) + posStart
   }
 
+  /**
+   * No easing function.
+   *
+   * @param {Object} object 
+   */
   _noease ({current, duration, posDelta, posStart}) {
     return posDelta + posStart
   }
